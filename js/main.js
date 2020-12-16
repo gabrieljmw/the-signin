69ea157bc37211270121967cc7e878f08429c0b8
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-12-16 16:21:16 -0500

    Added TO-DO

--- js/main.js
+++ js/main.js
@@ -46,3 +46,12 @@ form.addEventListener('submit', (event) => {
     });
 
 });
+
+//TO-DO
+/* Change the font-size of the #hello heading so that it's half the current size
+Consider the various ways we could validate both fields, including the use of a Boolean variable as a true or false status flag
+Try improving the user experience by adding/removing the .error class from an input.field immediately after the cursor leaves one of the individual <input> (rather than waiting for a submit event to occur)
+Consider the blur (opposite of focus) can be added individually to each input, but should be done so by writing one event listener/callback function and using a loop to apply to each of the input.field elements at the same time
+Allow the user to click the dark translucent background of the #modal block to have it close itself (but not the the form within it)
+Test this and see what happens when an element inside of another element (here, the form within the .modal) is clicked. Click events will "bubble" (propagate) up the DOM tree!
+Search for a way to stop an event (clicking the <form>) from bubbling to its parent (the .modal) *?
